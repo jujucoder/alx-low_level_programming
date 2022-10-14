a3f3ea2c599ceb8f0d6bf25aacf53af75cb331e3
okputu julius <juliusstan10@gmail.com>
2022-10-14 15:56:30 -0700

    3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:Always 0(succesfull)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+	}
+	while (alpha <= 'z')
+	{
+		putchar(toupper(alpha));
+	}
+
+	return (0);
+}
+
