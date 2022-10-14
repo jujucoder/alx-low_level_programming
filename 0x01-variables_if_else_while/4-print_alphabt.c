e915cb93ff1c17a8856714d3b6b5307ef020c4e8
okputu julius <juliusstan10@gmail.com>
2022-10-14 16:46:56 -0700

    4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ * Return:0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
