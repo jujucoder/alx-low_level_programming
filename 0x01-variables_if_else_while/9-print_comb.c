6d9a98f3053ca573bf8789c29e8b0265121410b6
okputu julius <juliusstan10@gmail.com>
2022-10-14 18:01:25 -0700

    9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+		if (d != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
