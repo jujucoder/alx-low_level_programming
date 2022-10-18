a0313582b02d5e9496cf72e1a0200e3ce2fd9abc
okputu julius <juliusstan10@gmail.com>
2022-10-18 16:26:26 -0700

    7 done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return last digit
+ *
+ * @l: value to be checked
+ *
+ * Return: last digit
+ */
+int print_last_digit(int l)
+{
+	return (l % 10);
+}
