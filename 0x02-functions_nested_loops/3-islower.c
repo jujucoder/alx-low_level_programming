4de239f5a8df2af1dcfd73bcfba38b638b4a2258
okputu julius <juliusstan10@gmail.com>
2022-10-18 15:46:04 -0700

    3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description:Program that checks for lowercase characters
+ *
+ * Return:1 if lowercase, 0 if uppercase
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' || c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
