51ced5d6e92f766827a1ad2e17b02e3e3ad1c9e0
okputu julius <juliusstan10@gmail.com>
2022-10-20 14:10:52 -0700

    3 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - print numbers from 0 - 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i < 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
