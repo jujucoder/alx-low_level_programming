e361d114048aacc9aca920895a02e43729a01c35
okputu julius <juliusstan10@gmail.com>
2022-10-24 13:23:56 -0700

    2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a str
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s[i] != '\0'; ++i);
+
+	return i;
+}
