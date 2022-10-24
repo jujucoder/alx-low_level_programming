66f24b126a5c16a068e2b7b65568fb920817219c
okputu julius <juliusstan10@gmail.com>
2022-10-24 12:37:09 -0700

    0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a variable to 98
+ *
+ * @n: var to be reset
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef
+#define
+
+void reset_to_98(int *n);
+int _putchar(char c);
+
+#endif
