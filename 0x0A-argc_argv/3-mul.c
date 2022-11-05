c15d5e549870df8e27fffe94379c75e622b4d8b9
okputu julius <juliusstan10@gmail.com>
2022-11-05 11:52:59 -0700

    3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num1;
+		int num2;
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	return (0);
+}
+
