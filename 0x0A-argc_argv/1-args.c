b25ba4dbce1490cb1f65fbbb3953c6d20145c9f6
okputu julius <juliusstan10@gmail.com>
2022-11-05 09:53:40 -0700

    2 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int argc, **argv)
+{
+	printf("%d", argc);
+	return (0);
+}
