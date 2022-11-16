075cd05112f1a0fdf18dd19b913f436c151647b3
okputu julius <juliusstan10@gmail.com>
2022-11-16 01:48:38 -0800

    0 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
+#endif
