c5a2a43415432642c8f441508afebd716f5c4ca2
okputu julius <juliusstan10@gmail.com>
2022-11-17 04:21:17 -0800

    1 done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ *  array_iterator - executes a function on each element of an array
+ *  @array: array
+ *  @size: array size
+ *  @action: function to be executed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t count;
+
+	for (count = 0; count < size; count++)
+	{
+		action(*(array + count));
+	}
+}
