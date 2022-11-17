17c3d691ddd796fde6bcf2388b36aaaf6bf5d06d
okputu julius <juliusstan10@gmail.com>
2022-11-17 04:32:03 -0800

    1 done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t count;
-	if (action == NULL || size == NULL)
+
+	if (action == NULL || size < 0 || array == NULL)
 		return;
 	for (count = 0; count < size; count++)
 	{
-		if (*(array + count) == NULL)
-			return;
 		action(*(array + count));
 	}
 }
