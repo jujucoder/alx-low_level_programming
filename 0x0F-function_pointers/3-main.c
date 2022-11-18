f235e0cba99c7cd501fe9d0c0c54b3e356f3cbcb
okputu julius <juliusstan10@gmail.com>
2022-11-17 18:20:07 -0800

    fix betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,8 @@ int main(int __attribute__((__unused__)) argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 || num2 == 0)
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
