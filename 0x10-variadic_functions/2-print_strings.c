40564e5063001e20ba8c0d631d0b3aa24e0b37d1
okputu julius <juliusstan10@gmail.com>
2022-11-18 06:55:01 -0800

    1 done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_numbers - print numbers with a separator
+ * @separator: separator
+ * @int: number of args
+ * @...: variable num of args
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i, j;
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(num, int);
+		printf("%d", j);
+		if (separator == NULL)
+			continue;
+		printf("%s", separator);
+	}
+	va_end(num);
+	printf("\n");
+}
