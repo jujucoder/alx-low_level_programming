7425824425e59b149071f14f0d5959fb8f871fcd
okputu julius <juliusstan10@gmail.com>
2022-11-27 18:34:59 +0100

    task 0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns no of nodes of linked list
+ * @h: linked lists
+ * return: no of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
