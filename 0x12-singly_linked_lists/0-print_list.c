df72b70d04e1cd3b5c2cc6c0e3832ab970f528d8
okputu julius <juliusstan10@gmail.com>
2022-11-27 18:13:47 +0100

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - print contents of a list
+ * @h: - linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] (%s)\n", h->len, h->str);
+		h = h->next;
+	}
+	return (count);
+}
