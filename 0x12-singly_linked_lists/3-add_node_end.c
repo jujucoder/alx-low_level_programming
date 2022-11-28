14410075e7b6fc0d9419c9fb24cc7dec4db7c093
okputu julius <juliusstan10@gmail.com>
2022-11-28 17:42:56 +0100

    3 fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,31 +11,32 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *last;
 	list_t *current;
-	int count;
+	unsigned int count;
 
-	last = malloc(sizeof(list_t));
 	for (count = 0; str[count]; count++)
 	;
 
+	last = malloc(sizeof(list_t));
 	if (last == NULL)
 		return (NULL);
+
 	last->str = strdup(str);
 	if (last->str == NULL)
 		return (NULL);
+
 	last->len = count;
 	last->next = NULL;
 	
 	if (*head == NULL)
 	{
 		*head = last;
-		return (*head);
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-
-
-	current->next = last;
-
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = last;
+	}
 	return (*head);
 }
