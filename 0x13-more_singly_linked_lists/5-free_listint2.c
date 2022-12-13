87f6bcadfd9f28450138715623986dbcf51afbd6
okputu julius <juliusstan10@gmail.com>
2022-12-13 14:06:08 -0800

    5 fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,14 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 
 	
