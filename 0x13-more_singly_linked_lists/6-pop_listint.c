2a860d4ae396d0c86bab9b52ed668a8d0aaaed70
okputu julius <juliusstan10@gmail.com>
2022-12-16 01:56:48 -0800

    6 done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,10 +10,12 @@ int pop_listint(listint_t **head)
 	int n;
 	listint_t *temp;
 
+	if (head == NULL)
+		return (0);
+
 	n = (*head)->n;
 	temp = *head;
 	*head = (*head)->next;
 	free(temp);
-	temp = NULL;
 	return (n);
 }
