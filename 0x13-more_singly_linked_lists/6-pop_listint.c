715e58b1a95160e056e3d7f6c80c8ef5ee1500f8
okputu julius <juliusstan10@gmail.com>
2022-12-16 01:52:39 -0800

    6 done

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * pop_listint - removes head of node
+ * @head: pointer to adress of first element in list
+ * Return: element of removed node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
